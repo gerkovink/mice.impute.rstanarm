d8f4903d768c5ec3ae3154c211f330c59a7b200e
RianneSchouten <r.m.schouten@uu.nl>
2016-11-30 19:48:08 -0500

    Run simulation rho 0.5 + qr  = TRUE
    QR = TRUE was a tip from Ben, it didn't have any effect though.

---
+++ Sim1_rho5_qrTRUE/Functions/mice.impute.rstanarm.R
@@ -0,0 +1,13 @@
+.stan.draw <- function(y, ry, x, ridge = 1e-05, ...){
+  noprint <- capture.output(fit <- stan_glm(y ~ x[,-1], algorithm = "optimizing", QR = TRUE))
+  sigma.star <- sqrt(sum((residuals(fit))^2)/rchisq(1, fit$df.residual))
+  parm <- list(coef(fit), sigma.star)
+  names(parm) <- c("beta", "sigma")
+  return(parm)
+}
+
+mice.impute.rstanarm <- function(y, ry, x, ...){
+  x <- cbind(1, as.matrix(x))
+  parm <- .stan.draw(y, ry, x)
+  return(x[!ry, ] %*% parm$beta + rnorm(sum(!ry)) * parm$sigma)
+}
